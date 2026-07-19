36621b3fe2adecca6f245bc1a0fde2e685cedfc9
UserElen <balasaynelen@gmail.com>
2026-07-19 06:00:21 +0400

    Create and close a basic TCP socket

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
 
 int main()
 {
 	std::cout << "Chat starting..." << std::endl;
+	int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+	
+	if(socket_fd < 0) {
+		std::cout << "Failed" << std::endl;
+		return -1;
+	}
 
+	close(socket_fd);
 	return 0;
 }
